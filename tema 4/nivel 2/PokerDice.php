b0c2d9b63a29fb39b74de35181948c3b6e768eaa
grinxy <moespon@gmail.com>
2024-01-22 18:27:58 +0100

    DiceFaces as static variable

--- tema 4/nivel 2/PokerDice.php
+++ tema 4/nivel 2/PokerDice.php
@@ -5,19 +5,19 @@ declare(strict_types=1);
 
 class PokerDice
 {
-    private const DICE_FACES = ["As", "K", "Q", "J", "7", "8"];   //constante no variable. Todos los dados tienen los mismos lados SIEMPRE.
+    private static $diceFaces = ["As", "K", "Q", "J", "7", "8"];   //como constante no variable, aplica a cada objeto. Como static es compartida por todos los objetos de la clase.
     private $lastThrow;
     private static $totalThrows = 0;   //static --> todas las instancias acceden y al pertenecer a la clase no a los objetos, mantiene su valor entre diferentes llamadas.
 
 
     public function getDiceFaces(): array
     {
-        return self::DICE_FACES;
+        return static::$diceFaces;
     }
 
     public function throw(): string
     {
-        $throw = self::DICE_FACES[array_rand(self::DICE_FACES, 1)];
+        $throw = static::$diceFaces[array_rand(static::$diceFaces, 1)];
         $this->lastThrow = $throw;
         self::$totalThrows++;
         return $throw;
